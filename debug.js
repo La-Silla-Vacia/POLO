7af12c897195d3883bf597d04042df7adbb05354
Wietse Neven <hello@wietseneven.com>
2017-04-18 17:27:51 -0500

    excluded it from preact

--- debug.js
+++ debug.js
@@ -4,6 +4,8 @@ var lsv = require('lsv-interactive');
 import { h, render } from 'preact';
 import Base from './src/base';
 
+import script from './src/script.js';
+
 require("./src/base.css"); // this goes outside the callback since otherwise the interactive sometimes fires before the CSS is fully loaded
 require("./src/global.css");
 
@@ -17,7 +19,13 @@ lsv("moving-of-politics", function (interactive) {
 
   //MARKUP
   render((
-    <Base meta={interactive} />
+    <Base callback={changeYear} meta={interactive} />
   ), interactive.el);
+  // interactive.el.innerHTML = require("./src/base.html")();
 
+  function changeYear(year) {
+    console.log('abc', year);
+  }
+
+  new script();
 }, true); // change this last param to true if you want to skip the DOM checks
